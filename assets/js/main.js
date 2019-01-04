21abe8621cfd92addd167259bcf4abb56ee3b7bc
Liad <liad.magen@gmail.com>
2019-01-04 12:34:41 +0100

    added email validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -338,7 +338,18 @@
 
 						});
 
+
+
+		function validateEmail(email) {
+			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(String(email).toLowerCase());
+		}
+						
 		$('#contactform').on('submit', function() {
+			if (!validateEmail($('#email').val())) {
+				return false;
+			}
+
 			$.ajax({
 				type: 'POST',
 				url: 'https://keepcurrent-xprs.herokuapp.com/message',
